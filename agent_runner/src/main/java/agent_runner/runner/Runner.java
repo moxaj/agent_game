21f9d0efddc38351b29a6ff153907821abb2b584
moxaj <viktor.magyari92@gmail.com>
2018-04-13 13:01:12 +0200

    Runner javadocs [skip ci]

--- agent_runner/src/main/java/agent_runner/runner/Runner.java
+++ agent_runner/src/main/java/agent_runner/runner/Runner.java
@@ -1,5 +1,11 @@
 package agent_runner.runner;
 
+/**
+ * Represents an entity which can run a simulation.
+ */
 public interface Runner {
+    /**
+     * Runs the simulation.
+     */
     void run();
 }
